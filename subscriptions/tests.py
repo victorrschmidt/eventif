8c99c69098053e46be169ef4177467fde0de97ce
Aluno <vicoschmidt@gmail.com>
2024-09-30 17:17:25 -0300

    Iniciando app inscrição

---
+++ subscriptions/tests.py
@@ -0,0 +1,25 @@
+from django.test import TestCase
+
+class SubscribeTest(TestCase):
+    def setUp(self):
+        self.response = self.client.get('inscricao/')
+
+    def test_get(self):
+        self.assertEqual(200, self.response.status_code)
+    
+    def test_template(self):
+        self.assertTemplateUsed(self.response, 'subscriptions/subscription_form.html')
+    
+    def test_html(self):
+        self.assertContains(self.response, '<form')
+        self.assertContains(self.response, '<input', 6)
+        self.assertContains(self.response, 'type="text"', 3)
+        self.assertContains(self.response, 'type="email"')
+        self.assertContains(self.response, 'type="submit"')
+    
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_has_form(self):
+        form = self.response.context['form']
+        self.assertSequenceEqual(['name', 'cpf', 'email', 'phone'], list(form.fields))
