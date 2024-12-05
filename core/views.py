9ba7a63f308056efc123e94095c8118ba34ffc3f
Victor Schmidt <vicoschmidt@gmail.com>
2024-12-05 10:09:54 -0300

    Speakers

--- core/views.py
+++ core/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from core.models import Speaker
 
 
 def home(request):
-    return render(request, 'index.html')
+    speakers = [
+        {'name': 'Grace Hopper', 'photo': 'https://abre.ai/hopper-pic'},
+        {'name': 'Alan Turing', 'photo': 'https://abre.ai/turing-pic'}
+    ]
+    return render(request, 'index.html', {'speakers': speakers})
+
+
+def speaker_detail(request, slug):
+    speaker = get_object_or_404(Speaker, slug=slug)
+    return render(request, 'core/speaker_detail.html', {'speaker': speaker})
